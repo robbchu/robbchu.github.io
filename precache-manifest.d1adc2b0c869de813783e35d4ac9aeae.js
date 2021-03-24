a051ec815907a8b58724442f0332e83d292fecc3
robbchu <robertchu921@gmail.com>
2021-03-24 19:40:06 -0400

    Updates

---
+++ precache-manifest.d1adc2b0c869de813783e35d4ac9aeae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27a9319b0e4fb70b32a131143537b2a",
+    "url": "/juichu/index.html"
+  },
+  {
+    "revision": "cfd66e53ff6c68870b2c",
+    "url": "/juichu/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/juichu/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/juichu/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd66e53ff6c68870b2c",
+    "url": "/juichu/static/js/main.5c416b8f.chunk.js"
+  },
+  {
+    "revision": "08c48326f5e3b511b22a",
+    "url": "/juichu/static/js/runtime-main.d4467a7f.js"
+  },
+  {
+    "revision": "83c5cda5f96efe57296deb7259578836",
+    "url": "/juichu/static/media/inq.83c5cda5.jpg"
+  },
+  {
+    "revision": "ce05ef2cd9faaef0a582da558346c78e",
+    "url": "/juichu/static/media/ntu.ce05ef2c.jpg"
+  },
+  {
+    "revision": "79e96d7a025d025ce138b2cf370242e1",
+    "url": "/juichu/static/media/resume.79e96d7a.pdf"
+  },
+  {
+    "revision": "7b3f721a98afb952c242e56971b9d9ba",
+    "url": "/juichu/static/media/robertchu.7b3f721a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cea4977738cbf6c8696d51b0d2a77223.js"
+  "/juichu/precache-manifest.d1adc2b0c869de813783e35d4ac9aeae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/juichu/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
